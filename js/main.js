1622f71bfe3656351b74b462cc99dd8572f925af
sushkova <e.isxakova@gmail.com>
2021-06-01 00:22:15 +0300

    Правки синтаксиса

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 function getRandomIntInclusive(min, max) {
 
-    if (min < 0 || max < 0) {
-        return 'Некорректный диапазон';
-    };
+  if (min < 0 || max < 0) {
+    return 'Некорректный диапазон';
+  }
 
-    if (min >= max) {
-        return 'Некорректный диапазон';
-    } else {
+  if (min >= max) {
+    return 'Некорректный диапазон';
+  } else {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-};
+  };
+}
 
 getRandomIntInclusive();
 
 // Функция для проверки максимальной длины строки
 function checkStringLength (string, maxLength) {
-if(string <= maxLength) {
-return true;
-};
+  if(string <= maxLength) {
+    return true;
+  }
 
-return false;
-};
+  return false;
+}
 
 checkStringLength();
 
