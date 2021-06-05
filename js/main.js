e297479ed407259b7c69f82e70d7902a6a6d7230
sushkova <e.isxakova@gmail.com>
2021-06-05 16:56:57 +0300

    Внесла правки, но все равно не работает

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const MESSAGE = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAME = ['Оля', 'Петя', 'Коля', 'Саша', 'Катя', 'Ира'];
+
+const DESCRIPTION = [
+  'Приятно провожу вечер',
+  'Скорее бы выходные!',
+  'Очень много работы',
+  'Просто хорошее настроение',
+  'Отдыхаю с друзьями',
+  'Инстаграм для бумеров',
+];
+
+const USERS_POSTS_COUNT = 25;
+
 function getRandomIntInclusive(min, max) {
   const isBothNumbers = typeof min === 'number' && typeof max === 'number';
   if (!isBothNumbers) {
@@ -21,50 +42,30 @@ function checkStringLength(string, maxLength) {
 }
 checkStringLength();
 
-const DESCRIPTION = [
-  'Приятно провожу вечер',
-  'Скорее бы выходные!',
-  'Очень много работы',
-  'Просто хорошее настроение',
-  'Отдыхаю с друзьями',
-  'Инстаграм для бумеров',
-];
-
-const MESSAGE = ['Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-const NAME = ['Оля', 'Петя', 'Коля', 'Саша', 'Катя', 'Ира'];
-
-const USERS_POSTS_COUNT = 25;
-
 const createUserComment = () => {
-  const randomIdComment = getRandomIntInclusive();
-  /* const randomAvatar = getRandomIntInclusive(1, 6);*/
+  const randomIdComment = getRandomIntInclusive(1, 200);
+  const randomAvatar = getRandomIntInclusive(1, 6);
   const randomMessage = getRandomIntInclusive(0, MESSAGE.length - 1);
   const randomName = getRandomIntInclusive(0, NAME.length - 1);
-
   return {
     id: randomIdComment,
-    /*avatar: 'img/avatar-' + randomAvatar + '.svg',*/
+    avatar: `img/avatar-${randomAvatar}.svg`,
     message: MESSAGE[randomMessage],
     name: NAME[randomName],
   };
 };
 
+createUserComment();
+
 const createUserPost = () => {
   const randomId = getRandomIntInclusive(1, 25);
-  /*const randomIndexFoto = getRandomIntInclusive(1, 25);*/
+  const randomIndexFoto = getRandomIntInclusive(1, 25);
   const randomDescription = getRandomIntInclusive(0, DESCRIPTION.length - 1);
   const randomLike = getRandomIntInclusive(15, 200);
 
   return {
     id: randomId,
-    /*url: 'photos/' + randomIndexFoto + '.jpg',*/
+    url: `photos/${randomIndexFoto}.jpg`,
     description: DESCRIPTION[randomDescription],
     likes: randomLike,
     comments: createUserComment(),
