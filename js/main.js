e9932d6d0e69cddf2db6deb756702eedb8a3077c
sushkova <e.isxakova@gmail.com>
2021-06-12 00:09:32 +0300

    Добавила создание массива комментариев и добавила константы

--- js/main.js
+++ js/main.js
@@ -1,74 +1,98 @@
-const MESSAGE = ['Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+const MESSAGES = ['Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const NAME = ['Оля', 'Петя', 'Коля', 'Саша', 'Катя', 'Ира'];
+const NAMES = ['Оля', 'Петя', 'Коля', 'Саша', 'Катя', 'Ира'];
 
-const DESCRIPTION = [
-  'Приятно провожу вечер',
-  'Скорее бы выходные!',
-  'Очень много работы',
-  'Просто хорошее настроение',
-  'Отдыхаю с друзьями',
-  'Инстаграм для бумеров',
+const DESCRIPTIONS = [
+    'Приятно провожу вечер',
+    'Скорее бы выходные!',
+    'Очень много работы',
+    'Просто хорошее настроение',
+    'Отдыхаю с друзьями',
+    'Инстаграм для бумеров',
 ];
 
-const USERS_POSTS_COUNT = 25;
+const USERS_POSTS_COUNTS = 25;
+const MIN_COMMENTS_ID = 1;
+const MAX_COMMENTS_ID = 200;
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+const MIN_POST_ID = 1;
+const MAX_POST_ID = 25;
+const MIN_INDEX_FOTO = 1;
+const MAX_INDEX_FOTO = 25;
+const MIN_LIKE = 15;
+const MAX_LIKE = 200;
+const USERS_COMMENTS_COUNTS = 5;
 
 function getRandomIntInclusive(min, max) {
-  const isBothNumbers = typeof min === 'number' && typeof max === 'number';
-  if (!isBothNumbers) {
-    return 'Введенные значения должны быть числами';
-  }
-  if ((min < 0 || max < 0) || min >= max) {
-    return 'Некорректный диапазон';
-  }
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
+    const isBothNumbers = typeof min === 'number' && typeof max === 'number';
+    if (!isBothNumbers) {
+        return 'Введенные значения должны быть числами';
+    }
+    if ((min < 0 || max < 0) || min >= max) {
+        return 'Некорректный диапазон';
+    }
+    const newMin = Math.ceil(min);
+    const newMax = Math.floor(max);
+    return Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
 }
-getRandomIntInclusive();
+getRandomIntInclusive(3, 56);
 
 function checkStringLength(string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  }
+    if (string.length <= maxLength) {
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
+checkStringLength('Hi', 15);
+
+
 const createUserComment = () => {
-  const randomIdComment = getRandomIntInclusive(1, 200);
-  const randomAvatar = getRandomIntInclusive(1, 6);
-  const randomMessage = getRandomIntInclusive(0, MESSAGE.length - 1);
-  const randomName = getRandomIntInclusive(0, NAME.length - 1);
-  return {
-    id: randomIdComment,
-    avatar: `img/avatar-${randomAvatar}.svg`,
-    message: MESSAGE[randomMessage],
-    name: NAME[randomName],
-  };
+    const randomIdComment = getRandomIntInclusive(MIN_COMMENTS_ID, MAX_COMMENTS_ID); //проблема с повторением id. исправить с помощью map
+    const randomAvatar = getRandomIntInclusive(MIN_AVATAR, MAX_AVATAR);
+    const randomMessage = getRandomIntInclusive(0, MESSAGES.length - 1);
+    const randomName = getRandomIntInclusive(0, NAMES.length - 1);
+    return {
+        id: randomIdComment,
+        avatar: `img/avatar-${randomAvatar}.svg`,
+        message: MESSAGES[randomMessage],
+        name: NAMES[randomName],
+    };
 };
 
 createUserComment();
 
-const createUserPost = () => {
-  const randomId = getRandomIntInclusive(1, 25);
-  const randomIndexFoto = getRandomIntInclusive(1, 25);
-  const randomDescription = getRandomIntInclusive(0, DESCRIPTION.length - 1);
-  const randomLike = getRandomIntInclusive(15, 200);
+const arrayComments = new Array(USERS_COMMENTS_COUNTS).fill(null).map(() => createUserComment());
 
-  return {
-    id: randomId,
-    url: `photos/${randomIndexFoto}.jpg`,
-    description: DESCRIPTION[randomDescription],
-    likes: randomLike,
-    comments: createUserComment(),
-  };
+console.log(arrayComments);
+
+
+const createUserPost = () => {
+    const randomId = getRandomIntInclusive(MIN_POST_ID, MAX_POST_ID); //проблема с повторением id. исправить с помощью map
+    const randomIndexFoto = getRandomIntInclusive(MIN_INDEX_FOTO, MAX_INDEX_FOTO);
+    const randomDescription = getRandomIntInclusive(0, DESCRIPTIONS.length - 1);
+    const randomLike = getRandomIntInclusive(MIN_LIKE, MAX_LIKE);
+
+    return {
+        id: randomId,
+        url: `photos/${randomIndexFoto}.jpg`,
+        description: DESCRIPTIONS[randomDescription],
+        likes: randomLike,
+        comments: arrayComments,
+    };
 };
 
-const usersPosts = new Array(USERS_POSTS_COUNT).fill(null).map(() => createUserPost());
+createUserPost();
+
+
+const usersPosts = new Array(USERS_POSTS_COUNTS).fill(null).map(() => createUserPost());
+
+console.log(usersPosts);
