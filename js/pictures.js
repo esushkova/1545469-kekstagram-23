ba694f1af7ecaaba76c3d54a6e7d6b4d825d19c4
sushkova <e.isxakova@gmail.com>
2021-10-05 17:24:27 +0300

    Переписала цикл с for на forEach в pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -5,13 +5,13 @@ const createThumbnails = (images) => {
   let template = picturesTemplateFragment.querySelector('.picture');
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < images.length; i++) {
-    let element = template.cloneNode(true);
-    element.querySelector('.picture__img').src = images[i].url;
-    element.querySelector('.picture__likes').textContent = images[i].likes;
-    element.querySelector('.picture__comments').textContent = images[i].comments;
-    fragment.appendChild(element);
-  };
+  images.forEach(item => {
+  let element = template.cloneNode(true);
+  element.querySelector('.picture__img').src = item.url;
+  element.querySelector('.picture__likes').textContent = item.likes;
+  element.querySelector('.picture__comments').textContent = item.comments.length;
+  fragment.appendChild(element);
+});
 
   picturesBlock.appendChild(fragment)
 }
