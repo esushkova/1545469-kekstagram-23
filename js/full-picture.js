7a76d5cc1d314bc9ba9653675851fe62100b72aa
sushkova <e.isxakova@gmail.com>
2021-10-06 23:56:50 +0300

    Отрисуй меня полностью (часть 2)

--- js/full-picture.js
+++ js/full-picture.js
@@ -3,11 +3,33 @@ const createFullPhoto = (array, id) => {
   let fullPhotoImg = document.querySelector('.big-picture__img').children[0];
   let fullPhotoLikes = document.querySelector('.likes-count');
   let fullPhotoCommentsCount = document.querySelector('.comments-count');
+  let photoDescription = document.querySelector('.social__caption');
 
   fullPhotoImg.src = array[id].url;
   fullPhotoLikes.textContent = array[id].likes;
   fullPhotoCommentsCount.textContent = array[id].comments.length;
+  photoDescription.textContent = array[id].description;
+
+  //список комментариев
+  let commentList = document.querySelector('.social__comments');
+  let commentTemplate = commentList.querySelector('.social__comment');
+  const fragment = document.createDocumentFragment();
+
+  array[id].comments.forEach(item => {
+
+    let element = commentTemplate.cloneNode(true)
+
+    element.children[0].src = item.avatar;
+    element.children[0].alt = item.name;
+    element.children[1].textContent = item.message;
+
+    fragment.appendChild(element);
+  })
+
+  commentList.innerHTML = '';
+  commentList.appendChild(fragment)
+
 }
 
+export { createFullPhoto }
 
-  export {createFullPhoto}
--- js/main.js
+++ js/main.js
@@ -3,23 +3,32 @@ import { createThumbnails } from './pictures.js';
 import { createFullPhoto } from './full-picture.js';
 
 let newArray = createPhotoArray();
+console.log(newArray)
 
 createThumbnails(newArray);
 
-let fullPhotoWindow = document.querySelector('.big-picture');
-let elements = document.querySelectorAll('.picture');
-let close = document.querySelector('.big-picture__cancel');
+let thumbnailsElements = document.querySelectorAll('.picture');
+let closeButton = document.querySelector('.big-picture__cancel');
 
-elements.forEach(element => {
+thumbnailsElements.forEach(element => {
 
   element.addEventListener('click', function () {
-        let id = element.id - 1;
-        fullPhotoWindow.classList.remove('hidden');
+    let id = element.id - 1;
+
+    document.querySelector('.big-picture').classList.remove('hidden');
+
+    document.querySelector('.social__comment-count').classList.add('hidden');
+    document.querySelector('.comments-loader').classList.add('hidden');
+    document.querySelector('body').classList.add('modal-open');
 
     createFullPhoto(newArray, id)
   })
 })
 
-close.addEventListener('click', function () {
-  fullPhotoWindow.classList.add('hidden');
+closeButton.addEventListener('click', function () {
+  document.querySelector('.big-picture').classList.add('hidden');
 })
+
+document.addEventListener('keydown', function () {
+  document.querySelector('.big-picture').classList.add('hidden');
+});
