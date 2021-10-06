7633c9e840979dba00b7b64593bcbc6b09ab2bd3
sushkova <e.isxakova@gmail.com>
2021-10-06 18:09:10 +0300

    Добавила атрибут id превьюшкам, начала делать окно full photo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
-import {createPhotoArray} from './data.js';
-import {createThumbnails} from './pictures.js';
+import { createPhotoArray } from './data.js';
+import { createThumbnails } from './pictures.js';
 
 console.log(createPhotoArray())
 
-createThumbnails(createPhotoArray())
+
+let newArray = createPhotoArray();
+
+createThumbnails(newArray);
+
+let close = document.querySelector('.big-picture__cancel');
+let fullPhotoWindow = document.querySelector('.big-picture');
+
+let elements = document.querySelectorAll('.picture');
+
+elements.forEach(element => {
+
+  element.addEventListener('click', function () {
+    fullPhotoWindow.classList.remove('hidden');
+
+    let id = element.id - 1;
+
+    let fullPhotoImg = fullPhotoWindow.querySelector('.big-picture__img').children[0];
+    let fullPhotoLikes = fullPhotoWindow.querySelector('.likes-count');
+    let fullPhotoCommentsCount = fullPhotoWindow.querySelector('.comments-count');
+
+    fullPhotoImg.src = newArray[id].url;
+    fullPhotoLikes.textContent = newArray[id].likes;
+    fullPhotoCommentsCount.textContent = newArray[id].comments.length;
+
+  })
+
+})
+
+
+close.addEventListener('click', function () {
+  fullPhotoWindow.classList.add('hidden');
+})
+
+
--- js/pictures.js
+++ js/pictures.js
@@ -4,9 +4,12 @@ const createThumbnails = (images) => {
   let picturesTemplateFragment = document.querySelector('#picture').content;
   let template = picturesTemplateFragment.querySelector('.picture');
   const fragment = document.createDocumentFragment();
+  template.setAttribute('id', '0');
+
 
   images.forEach(item => {
   let element = template.cloneNode(true);
+  element.id = item.id;
   element.querySelector('.picture__img').src = item.url;
   element.querySelector('.picture__likes').textContent = item.likes;
   element.querySelector('.picture__comments').textContent = item.comments.length;
