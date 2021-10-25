99bda92a4a1008acf7bc826ae54b0116fbc1c8b3
sushkova <e.isxakova@gmail.com>
2021-10-25 13:58:26 +0300

    Добавила проверку длины комментария, убрала мусор, отмена закрытия окна по ESC - не работает

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { createPhotoArray } from './data.js';
 import { createThumbnails } from './pictures.js';
-import { isEscEvent } from './util.js';
+import { isEscEvent, testUnique } from './util.js';
+import { MAX_DESCRIPTHION_LENHTH } from './variables.js';
 
 let newArray = createPhotoArray();
 console.log(newArray)
@@ -36,10 +37,11 @@ let openUploadButton = form.querySelector('#upload-file');
 let imgUploadForm = form.querySelector('.img-upload__overlay');
 let closeUploadButton = form.querySelector('#upload-cancel');
 
+let submitButton = document.querySelector('.img-upload__submit');
+
 let hashtagsInput = document.querySelector('.text__hashtags'); //поле хэштегов
 const re = /^#[A-Za-zА-Яа-я0-9]{1,19}$/;
-
-//let description = document.querySelector('.text__description'); //поле комментариев
+let description = document.querySelector('.text__description'); //поле комментариев
 
 openUploadButton.addEventListener('change', function () {
   imgUploadForm.classList.remove('hidden');
@@ -61,14 +63,7 @@ document.addEventListener('keydown', function (evt) {
 });
 
 //валидация поля хэштега
-function testUnique(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] === array[j]) return false;
-    }
-  }
-  return true;
-}
+
 
 hashtagsInput.addEventListener('input', function (evt) {
   let string = hashtagsInput.value;
@@ -102,86 +97,26 @@ hashtagsInput.addEventListener('input', function (evt) {
   }
 });
 
+//не работает отмена закрытия по ESC
 document.addEventListener('keydown', function (evt) {
-  if(hashtagsInput.focus([ { preventScroll: true } ])) {
-    evt.stopPropagation()
+  let focus = document.activeElement;
+  if(isEscEvent(evt)) {
+    if(focus === hashtagsInput) {
+          evt.stopPropagation;
   }
-})
-
-/*
-  if(!re.test(hashtagsInput.value)) {
-    hashtagsInput.setCustomValidity('Хештег должен начинаться с #, должен состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
-  } else {
-    hashtagsInput.setCustomValidity('');
-  }
+}
 });
 
-hashtagsInput.addEventListener('input', () => {
-  hashtagsInput.setCustomValidity('');
-})
-*/
 
-
-/*
-document.addEventListener('keydown', function (evt) {
-  if (isEscEvent(evt)) {
+//валидация комментария
+submitButton.addEventListener('click', function (evt) {
+  let valueLength = description.value.length
+  if (valueLength > MAX_DESCRIPTHION_LENHTH) {
     evt.preventDefault();
-    document.querySelector('.big-picture').classList.add('hidden');
+    description.setCustomValidity(`Максимальная длина комментария - 140 символов. Удалите лишние ${  valueLength - MAX_DESCRIPTHION_LENHTH } симв.`);
+  } else {
+    description.setCustomValidity('');
   }
+  description.reportValidity();
 });
-*/
 
-
-
-
-/*
-let closeButton = document.querySelector('.big-picture__cancel');
-let container = document.querySelector('.pictures');
-
-container.addEventListener('click', function (evt) {
-  if(evt.target.nodeName === "IMG") {
-  document.querySelector('.big-picture').classList.remove('hidden');
-    createFullPhoto(newArray, id)
-  }
-})
-
-closeButton.addEventListener('click', function () {
-  document.querySelector('.big-picture').classList.add('hidden');
-})
-*/
-
-
-
-/*
-let closeButton = document.querySelector('.big-picture__cancel');
-let thumbnailsElements = document.querySelectorAll('.picture');
-
-thumbnailsElements.forEach(element => {
-
-  element.addEventListener('click', function () {
-    let id = element.id - 1;
-
-    document.querySelector('.big-picture').classList.remove('hidden');
-
-    document.querySelector('.social__comment-count').classList.add('hidden');
-    document.querySelector('.comments-loader').classList.add('hidden');
-    document.querySelector('body').classList.add('modal-open');
-
-    createFullPhoto(newArray, id)
-
-    //обработчик закрытия по Esc навешивается на документ
-    //только после срабатывания открытия
-    document.addEventListener('keydown', function (evt) {
-      if (isEscEvent(evt)) {
-        evt.preventDefault();
-        document.querySelector('.big-picture').classList.add('hidden');
-      }
-    });
-  })
-})
-
-closeButton.addEventListener('click', function () {
-  document.querySelector('.big-picture').classList.add('hidden');
-})
-
-*/
