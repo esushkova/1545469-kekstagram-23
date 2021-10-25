5d53ca2b2b8221342709e2e463065877e43762ad
sushkova <e.isxakova@gmail.com>
2021-10-25 18:51:04 +0300

    Отображение комментариев под фото по 5 шт

--- js/pictures.js
+++ js/pictures.js
@@ -30,8 +30,6 @@ const createThumbnails = (images) => {
 
       document.querySelector('.big-picture').classList.remove('hidden');
 
-      document.querySelector('.social__comment-count').classList.add('hidden');
-      document.querySelector('.comments-loader').classList.add('hidden');
       document.querySelector('body').classList.add('modal-open');
 
       createFullPhoto(images, id);
@@ -41,28 +39,3 @@ const createThumbnails = (images) => {
 }
 
 export { createThumbnails };
-
-
-/*
-const createThumbnails = (images) => {
-  let picturesBlock = document.querySelector('.pictures');
-  let picturesTemplateFragment = document.querySelector('#picture').content;
-  let template = picturesTemplateFragment.querySelector('.picture');
-  const fragment = document.createDocumentFragment();
-  template.setAttribute('id', '0');
-
-  images.forEach(item => {
-  let element = template.cloneNode(true);
-  element.id = item.id;
-  element.querySelector('.picture__img').src = item.url;
-  element.querySelector('.picture__likes').textContent = item.likes;
-  element.querySelector('.picture__comments').textContent = item.comments.length;
-  fragment.appendChild(element);
-});
-
-  picturesBlock.appendChild(fragment)
-}
-
-export { createThumbnails };
-
-*/
