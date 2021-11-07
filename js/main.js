84fc0cb6db306faf4f13800f19eaf48df2ff16f8
sushkova <e.isxakova@gmail.com>
2021-11-07 18:18:26 +0300

    Добавила управление размером фотографии при редактировании

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { createPhotoArray } from './data.js';
 import { createThumbnails } from './pictures.js';
 import { isEscEvent, testUnique } from './util.js';
 import { MAX_DESCRIPTHION_LENHTH } from './variables.js';
+import './nouislider.js';
 
 let newArray = createPhotoArray();
 console.log(newArray)
@@ -97,39 +98,6 @@ console.log(item)
 
 };
 
-/*
-hashtagsInput.addEventListener('input', function (evt) {
-  let string = hashtagsInput.value;
-  let space = ' ';
-  let hashtagArray = string.split(space);
-  let unique = testUnique(hashtagArray);
-  for (let i = 0; i <= hashtagArray.length - 1; i++) {
-    let item = hashtagArray[i].toUpperCase();
-
-    if (!re.test(item)) {
-      evt.preventDefault();
-      hashtagsInput.setCustomValidity('Хештег должен начинаться с #, должен состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
-      return false;
-    }
-    if (hashtagArray.length > 5) {
-      evt.preventDefault();
-      hashtagsInput.setCustomValidity('Вы можете указать не больше пяти хештегов');
-      return false;
-    }
-    if (unique) {
-      hashtagsInput.setCustomValidity('');
-    } else {
-      evt.preventDefault();
-      hashtagsInput.setCustomValidity('Хештеги не могут повторяться');
-      return false;
-    }
-
-    hashtagsInput.setCustomValidity('');
-    hashtagsInput.reportValidity();
-  }
-});
-*/
-
 const checkDescription = (evt) => {
   let valueLength = description.value.length
   if (valueLength > MAX_DESCRIPTHION_LENHTH) {
@@ -150,12 +118,34 @@ const onSubmitButtonClick = (evt) => {
 submitButton.addEventListener('click', onSubmitButtonClick)
 
 
-//не работает отмена закрытия по ESC
-document.addEventListener('keydown', function (evt) {
-  let focus = document.activeElement;
-  if(isEscEvent(evt)) {
-    if(focus === hashtagsInput) {
-          evt.stopPropagation;
+
+//управление размером фотографии
+
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+const previewImage = document.querySelector('.img-upload__preview img');
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+const STEP = 25;
+
+scaleControlValue.setAttribute('value', `${MAX_SCALE}%`);
+previewImage.style.transform = `scale(${MAX_SCALE}%, ${MAX_SCALE}%)`;
+
+scaleControlBigger.addEventListener('click', () => {
+  const value = Number(scaleControlValue.value.replace(/[^-0-9-.]/, ''));
+  if(value < MAX_SCALE) {
+    scaleControlValue.setAttribute('value', (`${value + STEP}%`));
+    previewImage.style.transform = `scale(${value + STEP}%)`;
   }
-}
 });
+
+scaleControlSmaller.addEventListener('click', () => {
+  const value = Number(scaleControlValue.value.replace(/[^-0-9-.]/, ''));
+  if(value > MIN_SCALE) {
+    scaleControlValue.setAttribute('value', (`${value - STEP}%`));
+    previewImage.style.transform = `scale(${value - STEP}%)`;
+  }
+});
+
+
